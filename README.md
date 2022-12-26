488ad84bc13703eac0499e5a2b6ce84de43fd207
gabrielwere <weregabriel91@gmail.com>
2022-12-26 12:56:03 +0300

    refactor : cleaning up

--- README.md
+++ README.md
@@ -1,5 +1,27 @@
-#### FTP CLIENT(built for pyftpdlib for now)
+## SIMPLE PYFTPDLIB CLIENT
 
-For now it only supports the list command but works are in motion to add more commands.<br>
-This version of the code is abhorrent to anyone who reads so refactors are on the way.<br>
-So is this README.md(explaining how everything,including ftp concepts implemented work).
+A simple ftp client for pyftpdlib server.<br>
+The program was tested with pyftpdlib 1.5.4.<br>
+
+#### USAGE
+
+You need to have python and pyftpdlib installed first.<br> 
+On a linux machine,start pyftpdlib.
+
+```
+python3 -m pyftpdlib -p 21
+
+```
+
+Depending on which Linux distro you use,the command could be slightly different.You may need root priveleges(sudo),you may not need to specify which python version you are using,among other things.<br>
+
+On another terminal,run the commands
+
+```
+gcc client.c -o client
+./client your_ip_address
+
+```
+The files and folders available will then be listed.<br>
+
+
--- client.c
+++ client.c
@@ -8,129 +8,138 @@
 #include <ctype.h>
 
 void error(char *);
+int create_socket(int,struct hostent *);
+
 int main(int argc, char *argv[])
 {
 
-	int socket_file_descriptor;
+	if(argc==2){
+		int socket_file_descriptor;
+		int port = 21;
+		struct hostent *server = gethostbyname(argv[1]);
 
-	if ((socket_file_descriptor = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-		error("could not create socket");
-	else
-	{
+		if ((socket_file_descriptor = create_socket(port,server)) < 0)
+			error("could not create socket");
 
-		if (argc == 2)
+		printf("Connected to %s:%d\n", argv[1], port);
+
+		//LOG IN TO THE FTP SERVER
+		//THE PROGRAM LOGS YOU IN WITH THE 'ANONYMOUS' USERNAME
+		//AND A BLANK PASSWORD
+
+		//n is the number of characters read/written
+		//by a read/write command
+		int n;
+
+		char response_220[50];
+		char response_331[50];
+		char response_230[50];
+		char response_227[50];
+
+		bzero(response_220,sizeof(response_220));
+		bzero(response_331,sizeof(response_331));
+		bzero(response_230,sizeof(response_230));
+		bzero(response_227,sizeof(response_227));
+
+		if ((n = read(socket_file_descriptor, response_220, 50)) < 0)
+			error("did not receive any data");
+		printf("%s\n",response_220);
+		
+
+		if (strncmp(response_220, "220", 3) == 0)
 		{
-			// change the port number if you wish to
-			int port_number = 21;
+			//send username to the password
+			char username[] = "USER anonymous\r\n";
+			if ((n = write(socket_file_descriptor, username, strlen(username))) < 0)
+				error("did not send any data");
 
-			struct hostent *server = gethostbyname(argv[1]);
-			if (server == NULL)
-				error("no such server");
-
-			struct sockaddr_in server_address;
-			server_address.sin_family = AF_INET;
-			server_address.sin_port = htons(port_number);
-			bcopy((char *)server->h_addr, (char *)&server_address.sin_addr.s_addr, server->h_length);
-
-			if ((connect(socket_file_descriptor, (struct sockaddr *)&server_address, sizeof(server_address))) < 0)
-				error("could not connect");
-
-			printf("Connected to %s:%d\n", argv[1], port_number);
-
-			char response[100];
-			int n;
-
-			bzero(response,sizeof(response));
-			if ((n = read(socket_file_descriptor, response, 100)) < 0)
+			if ((n = read(socket_file_descriptor, response_331, 50)) < 0)
 				error("did not receive any data");
+			printf("%s\n", response_331);
 
-			// server is ready for new user
-			if (strncmp(response, "220", 3) == 0)
+			// send password to server
+			if (strncmp(response_331, "331", 3) == 0)
 			{
-
-				char username[] = "USER anonymous\r\n";
-				if ((n = write(socket_file_descriptor, username, strlen(username))) < 0)
+				//send password to the server
+				char password[] = "PASS \r\n";
+				if ((n = write(socket_file_descriptor, password, strlen(password))) < 0)
 					error("did not send any data");
 
-				char enter_password[100];
-				bzero(enter_password,sizeof(enter_password));
-				if ((n = read(socket_file_descriptor, enter_password, 100)) < 0)
+				if ((n = read(socket_file_descriptor, response_230, 50)) < 0)
 					error("did not receive any data");
-				printf("%s\n", enter_password);
+				printf("%s\n", response_230);
 
-				// send password to server
-				if (strncmp(enter_password, "331", 3) == 0)
+				if(strncmp(response_230,"230",3)==0)
 				{
-					char password[] = "PASS \r\n";
-					if ((n = write(socket_file_descriptor, password, strlen(password))) < 0)
-						error("did not send any data");
-
-					char login_status[100];
-					bzero(response,sizeof(login_status));
-					if ((n = read(socket_file_descriptor, login_status, 100)) < 0)
-						error("did not receive any data");
-
-					printf("%s\n", login_status);
-					
-					char pasv[10] = "PASV \r\n";
+					char pasv[] = "PASV \r\n";
 					if ((n = write(socket_file_descriptor, pasv, strlen(pasv))) < 0)
 						error("did not send any data");
 
-					char server_addr[1000];
-					bzero(server_addr,sizeof(server_address));
-					if ((n = read(socket_file_descriptor, server_addr, 1000)) < 0)
+					
+					if ((n = read(socket_file_descriptor,response_227, 50)) < 0)
 						error("did not receive any data");
+					printf("%s\n", response_227);
 
-					printf("%s\n", server_addr);
-					int code;
-					int num1;
-					int num2;
-					int num3;
-					int num4;
+
+					//227 Entering passive mode (192,168,1,66,237,109).
+					//above is a sample 227 response
+					//we are only interested in the last two numbers which give us the port to connect to
+					//in this case 237 and 109(stored in num5 and num6 respectively)
 					int num5;
 					int num6;
 					char s1[20];
-					char s2[20];
-					char s3[20];
+					int ptr;
 
-					sscanf(server_addr,"%d %s %s %s (%d,%d,%d,%d,%d,%d)",&code,s1,s2,s3,&num1,&num2,&num3,&num4,&num5,&num6);
+					sscanf(response_227,"%d %s %s %s (%d,%d,%d,%d,%d,%d)",&ptr,s1,s1,s1,&ptr,&ptr,&ptr,&ptr,&num5,&num6);
 					int extracted_port = (num5 * 256)+num6;
 
 					int data_socket;
-
-					if ((data_socket = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+					if ((data_socket = create_socket(extracted_port,server)) < 0)
 						error("could not create socket");
 
-					struct hostent *server2 = gethostbyname(argv[1]);
-					if (server2 == NULL)
-						error("no such server");
-
-					struct sockaddr_in server2_address;
-					server2_address.sin_family = AF_INET;
-					server2_address.sin_port = htons(extracted_port);
-					bcopy((char *)server2->h_addr, (char *)&server2_address.sin_addr.s_addr, server2->h_length);
-
-					if ((connect(data_socket, (struct sockaddr *)&server2_address, sizeof(server2_address))) < 0)
-						error("could not connect");
-
-
-					char ls[]="LIST \r\n";
+					char ls[]="RETR hello.txt\r\n";
+					// char ls[]="LIST \r\n";
 					if((n = write(socket_file_descriptor,ls,strlen(ls))) < 0)
 						error("did send any data");
-
+					
 					char connection2[1000];
 					bzero(connection2,sizeof(connection2));
-					if ((n = read(data_socket, connection2, 1000)) < 0)
+					if ((n = read(socket_file_descriptor, connection2, 1000)) < 0)
+						error("did not receive any data");
+					printf("%s\n", connection2);
+
+					char connection3[10000];
+					bzero(connection3,sizeof(connection3));
+					if ((n = read(data_socket, connection3, 10000)) < 0)
 						error("did not receive any data");
 
-					printf("%s\n", connection2);
-					close(data_socket);
-				}
-			}
+					FILE *fp;
+					if((fp = fopen("hello.txt","w"))!=NULL){
+						fprintf(fp,"%s",connection3);
+					}
+					fclose(fp);
 
-			close(socket_file_descriptor);
+					char connection24[1000];
+					bzero(connection24,sizeof(connection24));
+					if ((n = read(socket_file_descriptor, connection24, 1000)) < 0)
+						error("did not receive any data");
+					printf("%s\n", connection24);
+
+
+					close(data_socket);
+
+				}else{
+					error("code 230 not received");
+				}
+			}else{
+				error("code 331 not received");
+			}
+		}else{
+			error("code 220 not received");
 		}
-		else
+		close(socket_file_descriptor);
+	
+	}else{
 			error("must provide host name");
 	}
 }
@@ -139,4 +148,23 @@ void error(char *error_message)
 {
 	perror(error_message);
 	exit(1);
+}
+
+int create_socket(int port,struct hostent *server){
+	int socket_file_descriptor;
+	if((socket_file_descriptor = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+		error("could not create socket");
+
+	if (server == NULL)
+		error("no such server");
+
+	struct sockaddr_in server_address;
+	server_address.sin_family = AF_INET;
+	server_address.sin_port = htons(port);
+	bcopy((char *)server->h_addr, (char *)&server_address.sin_addr.s_addr, server->h_length);
+
+	if ((connect(socket_file_descriptor, (struct sockaddr *)&server_address, sizeof(server_address))) < 0)
+		error("could not connect");
+	
+	return socket_file_descriptor;
 }
